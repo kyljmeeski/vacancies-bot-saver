a6074d1d59656bf4795750e9cdc4c9787d96e787
kyljmeeski <kyljmeeski@gmail.com>
2024-05-15 18:23:36 +0600

    feat: declare vacancies-to-store queue, create consumer of this queue

--- src/main/java/com/kyljmeeski/vacanciesbot/saver/Main.java
+++ src/main/java/com/kyljmeeski/vacanciesbot/saver/Main.java
@@ -1,6 +1,13 @@
 package com.kyljmeeski.vacanciesbot.saver;
 
+import com.kyljmeeski.rabbitmqwrapper.Consumer;
+import com.kyljmeeski.rabbitmqwrapper.PlainConsumer;
+import com.kyljmeeski.rabbitmqwrapper.Queues;
+import com.kyljmeeski.rabbitmqwrapper.RabbitQueue;
 import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 public class Main {
 
@@ -8,6 +15,17 @@ public class Main {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         factory.setPort(5672);
+
+        Queues queues = new Queues(factory);
+        try {
+            RabbitQueue toStoreQueue = queues.declare(
+                    "vacancies-to-store", false, false, false, null
+            );
+            Consumer consumer = new PlainConsumer(factory, toStoreQueue, System.out::println);
+            consumer.startConsuming();
+        } catch (IOException | TimeoutException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
