072e516b22c5f0e62937e39066c8f63e4156e8ea
kyljmeeski <kyljmeeski@gmail.com>
2024-05-17 21:20:05 +0600

    feat: establish connection to MongoDB; get database and pass to vacancies; create declare queue "vacancies-to-store", and start consuming it as defined in VacancyStoreJob

--- src/main/java/com/kyljmeeski/vacanciesbot/saver/Main.java
+++ src/main/java/com/kyljmeeski/vacanciesbot/saver/Main.java
@@ -4,6 +4,10 @@ import com.kyljmeeski.rabbitmqwrapper.Consumer;
 import com.kyljmeeski.rabbitmqwrapper.PlainConsumer;
 import com.kyljmeeski.rabbitmqwrapper.Queues;
 import com.kyljmeeski.rabbitmqwrapper.RabbitQueue;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
@@ -16,13 +20,23 @@ public class Main {
         factory.setHost("localhost");
         factory.setPort(5672);
 
-        Queues queues = new Queues(factory);
         try {
-            RabbitQueue toStoreQueue = queues.declare(
-                    "vacancies-to-store", false, false, false, null
-            );
-            Consumer consumer = new PlainConsumer(factory, toStoreQueue, System.out::println);
-            consumer.startConsuming();
+            Connection connection = factory.newConnection();
+
+            MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+            MongoDatabase database = mongoClient.getDatabase("vacancies");
+            Vacancies vacancies = new Vacancies(database);
+
+            Queues queues = new Queues(connection);
+            try {
+                RabbitQueue toStoreQueue = queues.declare(
+                        "vacancies-to-store", false, false, false, null
+                );
+                Consumer consumer = new PlainConsumer(connection, toStoreQueue, new VacancyStoreJob(vacancies));
+                consumer.startConsuming();
+            } catch (IOException | TimeoutException e) {
+                throw new RuntimeException(e);
+            }
         } catch (IOException | TimeoutException e) {
             throw new RuntimeException(e);
         }
